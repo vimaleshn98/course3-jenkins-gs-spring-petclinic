b0ffbe04282a6fca09387a42b7d8c59c21202051
vimalesh <vimaleshn198@gmail.com>
2024-12-12 20:50:29 +0530

    removed first name

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("chris");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
